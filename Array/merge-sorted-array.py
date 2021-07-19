a9d1520b3445a5d085fc8fd2e21ab85762cc2684
Karam <49024557+krhong23@users.noreply.github.com>
2021-07-19 23:16:06 +0900

    array / python / 21-07-19
    Runtime: 20 ms / Memory Usage: 13.4 MB
    
    https://leetcode.com/problems/merge-sorted-array/

---
+++ Array/merge-sorted-array.py
@@ -0,0 +1,27 @@
+'''
+problem
+
+You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1. 
+To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. 
+nums2 has a length of n.
+
+'''
+
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        for i in range(m + n):
+            if (i < m):
+                continue
+            nums1[i] = nums2[i - m]
+        nums1.sort()
