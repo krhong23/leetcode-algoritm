aa1445477272747471d34e8ff2515b6cd81368e1
Karam <49024557+krhong23@users.noreply.github.com>
2021-07-21 00:09:22 +0900

    array / python / 21-07-21
    Runtime: 20 ms / Memory Usage: 13.4 MB
    
    https://leetcode.com/problems/remove-element/

---
+++ Array/remove-element.py
@@ -0,0 +1,27 @@
+'''
+problem
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed.
+
+Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums.
+More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result.
+It does not matter what you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots of nums.
+'''
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        count = 0
+        for n in nums:
+            if n == val:
+                count += 1
+
+        for i in range(count):
+            nums.remove(val)
+
+        return len(nums)
