55590dec55ea385f4fecae445782488356c53ffb
Karam <49024557+krhong23@users.noreply.github.com>
2021-07-24 20:30:47 +0900

    Array, Simulation / Python
    [21-07-24] Runtime: 112ms / Memory Usage: 13.7 MB
    [21-07-13] Runtime: 160ms / Memory: 13.9 MB
    
    https://leetcode.com/problems/build-array-from-permutation/

--- Array/build-array-from-permutation.py
+++ Array/build-array-from-permutation.py
@@ -1,3 +1,11 @@
+'''
+problem
+Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+
+A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+'''
+
+
 class Solution(object):
     def buildArray(self, nums):
         """
@@ -5,3 +13,17 @@ class Solution(object):
         :rtype: List[int]
         """
         return [nums[nums[i]] for i in range(len(nums))]
+
+
+class EuclideanAlgorithmSolution(object):
+    def buildArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        q = len(nums)
+        for i, c in enumerate(nums):
+            nums[i] += q * (nums[c] % q)
+        for i, _ in enumerate(nums):
+            nums[i] //= q
+        return nums
