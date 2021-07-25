7eafe8e0ad2ee94d7903a73d46c48ee29551c9d2
krhong23 <krhong23@gmail.com>
2021-07-25 23:36:39 +0900

    Array / Python [21-07-25] Runtime: 288 ms / Memory Usage: 13.8 MB [21-07-14] Runtime: 296 ms / Memory Usage: 13.7 MB
    https://leetcode.com/problems/max-consecutive-ones/

--- Array/max-consecutive-ones.py
+++ Array/max-consecutive-ones.py
@@ -6,8 +6,8 @@ class Solution():
         :type nums: List[int]
         :rtype: int
         """
-        count = 0;
-        maxCount = 0;
+        count = 0
+        maxCount = 0
 
         for i in nums:
             if i == 1:
@@ -17,3 +17,20 @@ class Solution():
             else:
                 count = 0
         return maxCount
+
+
+class SimpleSolution():
+    def findMaxConsecutiveOnes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = 0
+        maxCount = 0
+        for num in nums:
+            if num == 1:
+                count += 1
+            else:
+                maxCount = max(count, maxCount)
+                count = 0
+        return max(count, maxCount)
