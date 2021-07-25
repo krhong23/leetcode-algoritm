ac8429fc5f10beaa27212c59e57a0f9d53ed0d57
krhong23 <krhong23@gmail.com>
2021-07-25 23:58:01 +0900

    Array, Sorting / Python [21-07-25] Runtime: 196 ms / Memory Usage: 15.7 MB [21-07-17] Runtime: 172 ms / Memory Usage: 15.5 MB
    https://leetcode.com/problems/squares-of-a-sorted-array/

--- Array/squares-of-a-sorted-array.py
+++ Array/squares-of-a-sorted-array.py
@@ -1,5 +1,8 @@
 # problem: Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
 
+import collections
+
+
 class Solution(object):
     def sortedSquares(self, nums):
         """
@@ -11,3 +14,22 @@ class Solution(object):
             array.append(n * n)
         array.sort()
         return array
+
+
+class DequeSolution(object):
+    def sortedSquares(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        answer = collections.deque()
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            left, right = abs(nums[l]), abs(nums[r])
+            if left > right:
+                answer.appendleft(left * left)
+                l += 1
+            else:
+                answer.appendleft(right * right)
+                r -= 1
+        return list(answer)
