b73ca5694b5e043fff5a8045469f6dc0f2391414
krhong23 <krhong23@gmail.com>
2021-07-29 23:25:33 +0900

    Array / Python / 21-07-29 Runtime: 24 ms / Memory Usage: 13.3 MB
    https://leetcode.com/problems/height-checker/

---
+++ Array/height-checker.py
@@ -0,0 +1,25 @@
+'''
+problem
+A school is trying to take an annual photo of all the students.
+The students are asked to stand in a single file line in non-decreasing order by height.
+Let this ordering be represented by the integer array expected where expected[i] is the expected height of the ith student in line.
+
+You are given an integer array heights representing the current order that the students are standing in.
+Each heights[i] is the height of the ith student in line (0-indexed).
+
+Return the number of indices where heights[i] != expected[i].
+'''
+
+
+class Solution(object):
+    def heightChecker(self, heights):
+        """
+        :type heights: List[int]
+        :rtype: int
+        """
+        count = 0
+        b = sorted(heights)
+        for i in range(len(heights)):
+            if heights[i] != b[i]:
+                count += 1
+        return count
