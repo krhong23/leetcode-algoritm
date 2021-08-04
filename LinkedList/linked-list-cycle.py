365b73955473065c7ef7bd6214156d869ac3e991
krhong23 <krhong23@gmail.com>
2021-08-04 22:38:43 +0900

    Linked List / Python / 21-08-04 Runtime: 40 ms / Memory Usage: 20.4 MB
    https://leetcode.com/problems/linked-list-cycle/

---
+++ LinkedList/linked-list-cycle.py
@@ -0,0 +1,34 @@
+'''
+problem
+
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer.
+Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+'''
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        slow = head
+        fast = head
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+            if slow is fast:
+                return True
+
+        return False
