bcf915a8acd6560cccf2cbe27d11762556f1a786
krhong23 <krhong23@gmail.com>
2021-08-05 22:31:08 +0900

    Linked List / Python / 21-08-05 Test Case (~ing) [-1,-7,7,-4,19,6,-9,-5,-2,-5] 6
    https://leetcode.com/problems/linked-list-cycle-ii/

---
+++ LinkedList/linked-list-cycle-ii.py
@@ -0,0 +1,36 @@
+'''
+problem
+
+Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer.
+Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Notice that you should not modify the linked list.
+'''
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution(object):
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        slow = head
+        fast = head
+        pos = -1
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow is fast:
+                return slow if slow is head else slow.next
+
+        return None
