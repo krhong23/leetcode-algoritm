7cca761d01ede9a03b1fa5d1a97b338c57ebc289
krhong23 <krhong23@gmail.com>
2021-08-28 07:47:00 +0900

    Array / Python / 21-08-28 Runtime: 5936 ms / Memory Usage: 15.7 MB
    https://leetcode.com/problems/single-number

---
+++ Array/single-number.py
@@ -0,0 +1,17 @@
+'''
+problem
+
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+'''
+
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        for n in nums:
+            if nums.count(n) < 2:
+                return n
