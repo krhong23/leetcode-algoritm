87af1fafccafb676677ca8bce641480fbfb21c21
krhong23 <krhong23@gmail.com>
2021-08-27 23:54:29 +0900

    Array / Python / 21-08-27 Runtime: 52 ms / Memory Usage: 13.7 MB
    https://leetcode.com/problems/shuffle-the-array/

---
+++ Array/shuffle-the-array.py
@@ -0,0 +1,20 @@
+'''
+problem
+
+Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+'''
+
+
+class Solution(object):
+    def shuffle(self, nums, n):
+        """
+        :type nums: List[int]
+        :type n: int
+        :rtype: List[int]
+        """
+        result = []
+        for i in range(n):
+            result.append(nums[i])
+            result.append(nums[n + i])
+        return result
