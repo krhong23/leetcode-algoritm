7863cb76c146954607c7f2113b9c157223ac4e80
krhong23 <krhong23@gmail.com>
2021-08-30 22:26:29 +0900

    Recursion / Python / 21-08-30 Runtime: 160 ms / Memory Usage: 21.2 MB
    https://leetcode.com/problems/reverse-string/

---
+++ Recursion/reverse-string.py
@@ -0,0 +1,9 @@
+# problem: Write a function that reverses a string. The input string is given as an array of characters s.
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        s.reverse()
