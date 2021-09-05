ae8d083e5c338865b32857fbf8e2980c55940ec2
krhong23 <krhong23@gmail.com>
2021-09-05 22:54:20 +0900

    String / Python / 21-09-05 Runtime: 40 ms / Memory Usage: 13.3 MB
    https://leetcode.com/problems/shuffle-string/

---
+++ String/shuffle-string.py
@@ -0,0 +1,21 @@
+'''
+problem
+
+Given a string s and an integer array indices of the same length.
+The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+Return the shuffled string.
+'''
+
+
+class Solution(object):
+    def restoreString(self, s, indices):
+        """
+        :type s: str
+        :type indices: List[int]
+        :rtype: str
+        """
+        result = [''] * len(s)
+        for i, char in enumerate(s):
+            result[indices[i]] = char
+
+        return ''.join(result)
