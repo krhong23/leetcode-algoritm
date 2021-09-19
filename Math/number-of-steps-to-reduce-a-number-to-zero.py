89098902d23f7b0d07d698972bc5a79fe46f7256
krhong23 <krhong23@gmail.com>
2021-09-19 13:11:11 +0900

    Math / Python / 21-09-19 Runtime: 27 ms / Memory Usage: 13.3 MB
    https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/

---
+++ Math/number-of-steps-to-reduce-a-number-to-zero.py
@@ -0,0 +1,23 @@
+'''
+problem
+
+Given an integer num, return the number of steps to reduce it to zero.
+
+In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+'''
+
+
+class Solution(object):
+    def numberOfSteps(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        count = 0
+        while num > 0:
+            if num % 2 == 0:
+                num = num / 2
+            else:
+                num = num - 1
+            count += 1
+        return count
