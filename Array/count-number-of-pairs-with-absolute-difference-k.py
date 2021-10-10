57c2ffc216ade9649400afcb177e65f1184a0b08
krhong23 <krhong23@gmail.com>
2021-10-10 21:07:48 +0900

    Array / Python / 21-10-10 Runtime: 212 ms / Memory Usage: 13.5 MB
    https://leetcode.com/problems/count-number-of-pairs-with-absolute-difference-k/

---
+++ Array/count-number-of-pairs-with-absolute-difference-k.py
@@ -0,0 +1,24 @@
+'''
+problem
+
+Given an integer array nums and an integer k, return the number of pairs (i, j) where i < j such that |nums[i] - nums[j]| == k.
+
+The value of |x| is defined as:
+    * x if x >= 0.
+    * -x if x < 0.
+'''
+
+
+class Solution(object):
+    def countKDifference(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        count = 0
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if abs(nums[i] - nums[j]) == k:
+                    count += 1
+        return count
