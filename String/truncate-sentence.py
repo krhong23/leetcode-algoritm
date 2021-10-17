da8895aeebccd01e5ab4f60db1a1b35c8a0a5c79
krhong23 <krhong23@gmail.com>
2021-10-17 22:06:11 +0900

    String / Python / 21-10-17 Runtime: 12 ms / Memory Usage: 13.7 MB
    https://leetcode.com/problems/truncate-sentence/

---
+++ String/truncate-sentence.py
@@ -0,0 +1,24 @@
+'''
+problem
+
+A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+Each of the words consists of only uppercase and lowercase English letters (no punctuation).
+
+For example, "Hello World", "HELLO", and "hello world hello world" are all sentences.
+You are given a sentence s​​​​​​ and an integer k​​​​​​. You want to truncate s​​​​​​ such that it contains only the first k​​​​​​ words.
+Return s​​​​​​ after truncating it.
+'''
+
+
+class Solution(object):
+    def truncateSentence(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        sentence = s.split()
+        result = ""
+        for i in range(k):
+            result += sentence[i] + " "
+        return result.rstrip()
