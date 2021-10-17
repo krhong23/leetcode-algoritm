dbf838ad5a15aa160b721733345f01295624f47c
krhong23 <krhong23@gmail.com>
2021-10-17 21:56:58 +0900

    String / Python / 21-10-17 Runtime: 16 ms / Memory Usage: 13.5 MB
    https://leetcode.com/problems/to-lower-case/

---
+++ String/to-lower-case.py
@@ -0,0 +1,10 @@
+# problem: Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+
+class Solution(object):
+    def toLowerCase(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return s.lower()
