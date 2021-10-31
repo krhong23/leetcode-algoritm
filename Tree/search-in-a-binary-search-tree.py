4c44fe2371c344cae112ed6021f3bc07b9e93afe
krhong23 <krhong23@gmail.com>
2021-10-31 19:43:32 +0900

    BST / Python / 21-10-31 Runtime: 64 ms / Memory Usage: 17.5 MB
    https://leetcode.com/problems/search-in-a-binary-search-tree/

---
+++ Tree/search-in-a-binary-search-tree.py
@@ -0,0 +1,30 @@
+'''
+problem
+
+You are given the root of a binary search tree (BST) and an integer val.
+
+Find the node in the BST that the node's value equals val and return the subtree rooted with that node.
+If such a node does not exist, return null.
+'''
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def searchBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        if root is None:
+            return None
+        elif root.val > val:
+            return self.searchBST(root.left, val)
+        elif root.val < val:
+            return self.searchBST(root.right, val)
+        return root
