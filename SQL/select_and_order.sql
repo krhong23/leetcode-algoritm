74d7343ac213739e36896b400405e770d7f3526d
rami <krhong23@gmail.com>
2022-08-15 20:44:25 +0900

    SQL(Select&Order) / MySQL / 22-08-15
    Runtime: 219 ms / Memory Usage: 0 MB
    https://leetcode.com/problems/swap-salary/

--- SQL/select_and_order.sql
+++ SQL/select_and_order.sql
@@ -15,3 +15,17 @@ ORDER BY employee_id;
   END AS bonus
     FROM Employees
 ORDER BY employee_id;
+
+-- 627. Swap Salary
+-- Write an SQL query to swap all 'f' and 'm' values (i.e., change all 'f' values to 'm' and vice versa) with a single update statement and no intermediate temporary tables.
+-- case1. Using CASE WHEN
+UPDATE Salary
+   SET
+       sex =
+       CASE sex
+           WHEN 'f' THEN 'm'
+           ELSE 'f'
+       END ;
+-- case2. Using IF
+UPDATE Salary
+   SET sex = IF(sex='f', 'm', 'f');
