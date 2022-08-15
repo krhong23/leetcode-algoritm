45cde7395af795c6294eea501feeb4f725c4223b
rami <krhong23@gmail.com>
2022-08-15 20:05:49 +0900

    SQL(Select) / MySQL / 22-08-15
    Runtime: 451 ms / Memory Usage: 0 MB
    https://leetcode.com/problems/find-customer-referee/

--- SQL/select.sql
+++ SQL/select.sql
@@ -3,15 +3,23 @@
 --  it has an area of at least three million (i.e., 3000000 km2), or
 --  it has a population of at least twenty-five million (i.e., 25000000).
 -- Write an SQL query to report the name, population, and area of the big countries.
-select name, population, area
-  from world
- where area >= 3000000
-    or population >= 25000000;
+SELECT name, population, area
+  FROM world
+ WHERE area >= 3000000
+    OR population >= 25000000;
 
 -- 1757. Recyclable and Low Fat Products
 -- Write an SQL query to find the ids of products that are both low fat and recyclable.
 -- Return the result table in any order.
-select product_id
-  from Products
- where low_fats = 'Y'
-   and recyclable = 'Y';
+SELECT product_id
+  FROM Products
+ WHERE low_fats = 'Y'
+   AND recyclable = 'Y';
+
+-- 584. Find Customer Referee
+-- Write an SQL query to report the names of the customer that are not referred by the customer with id = 2.
+-- Return the result table in any order.
+SELECT name
+  FROM Customer
+ WHERE referee_id != 2
+    OR referee_id IS NULL;
