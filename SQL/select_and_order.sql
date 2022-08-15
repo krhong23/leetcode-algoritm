003deff08ac2b63a3fab9687170c8ed302820724
rami <krhong23@gmail.com>
2022-08-15 21:02:41 +0900

    SQL(Select&Order) / MySQL / 22-08-15
    Runtime: 675 ms / Memory Usage: 0 MB
    https://leetcode.com/problems/delete-duplicate-emails/

--- SQL/select_and_order.sql
+++ SQL/select_and_order.sql
@@ -29,3 +29,27 @@ UPDATE Salary
 -- case2. Using IF
 UPDATE Salary
    SET sex = IF(sex='f', 'm', 'f');
+
+-- 196. Delete Duplicate Emails
+-- Write an SQL query to delete all the duplicate emails, keeping only one unique email with the smallest id.
+-- Note that you are supposed to write a DELETE statement and not a SELECT one.
+-- After running your script, the answer shown is the Person table.
+-- The driver will first compile and run your piece of code and then show the Person table.
+-- The final order of the Person table does not matter.
+-- case1. Using WHERE
+DELETE p1
+  FROM Person p1, Person p2
+ WHERE p1.email = p2.email
+   AND p1.id > p2.id;
+-- case2. Using Not In
+DELETE FROM Person
+ WHERE id NOT IN
+       (
+           SELECT *
+             FROM
+                  (
+                        SELECT MIN(id)
+                          FROM Person
+                      GROUP BY Email
+                  ) AS p
+       );
