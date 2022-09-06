5c2201f0eff7e1a36422050dfd00fa41d8b0c4f9
rami <krhong23@gmail.com>
2022-09-06 23:46:20 +0900

    SQL(String Processing Functions) / MySQL / 22-09-06
    Runtime: 472 ms / Memory Usage: 0 MB
    https://leetcode.com/problems/group-sold-products-by-the-date/

--- SQL/string_processing_functions.sql
+++ SQL/string_processing_functions.sql
@@ -7,3 +7,17 @@
   SELECT user_id, CONCAT(UPPER(SUBSTR(name, 1, 1)), LOWER(SUBSTR(name,2))) AS name
     FROM Users
 ORDER BY user_id ASC;
+
+-- 1484. Group Sold Products By The Date
+-- Write an SQL query to find for each date the number of different products sold and their names.
+-- The sold products names for each date should be sorted lexicographically.
+-- Return the result table ordered by sell_date.
+-- The query result format is in the following example.
+
+-- case1. Group Concat
+  SELECT sell_date,
+         COUNT(DISTINCT product) as num_sold,
+         GROUP_CONCAT(DISTINCT product) as products
+    FROM Activities
+GROUP BY sell_date
+ORDER BY sell_date, product;
