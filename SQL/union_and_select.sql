92fc16061d30d5e0ecc0d9db6906eceb7323ea93
rami <krhong23@gmail.com>
2022-09-12 19:13:52 +0900

    Union & Select / MySQL / 22-09-12
    Runtime: 453 ms / Memory Usage: 0 MB
    https://leetcode.com/problems/tree-node

--- SQL/union_and_select.sql
+++ SQL/union_and_select.sql
@@ -28,3 +28,24 @@ SELECT product_id, 'store2' as 'store' , store2 as 'price'
 SELECT product_id, 'store3' as 'store' , store3 as 'price'
   FROM Products
  WHERE store3 IS NOT NULL
+
+-- 608. Tree Node
+-- Each node in the tree can be one of three types:
+-- "Leaf": if the node is a leaf node.
+-- "Root": if the node is the root of the tree.
+-- "Inner": If the node is neither a leaf node nor a root node.
+-- Write an SQL query to report the type of each node in the tree.
+-- Return the result table ordered by id in ascending order.
+SELECT id, 'Root' as 'type'
+  FROM Tree
+ WHERE p_id IS NULL
+ UNION
+SELECT id, 'Inner' as 'type'
+  FROM Tree
+ WHERE p_id IS NOT NULL
+   AND id IN (SELECT p_id FROM Tree)
+ UNION
+SELECT id, 'Leaf' as 'type'
+  FROM Tree
+ WHERE p_id IS NOT NULL
+   AND id NOT IN (SELECT p_id FROM Tree WHERE p_id IS NOT NULL)
