86ba61a3e10232e817b423d556746ccf85afd63a
rami <krhong23@gmail.com>
2022-09-12 19:27:37 +0900

    Union & Select / MySQL / 22-09-12
    Runtime: 230 ms / Memory Usage: 0 MB
    https://leetcode.com/problems/second-highest-salary

--- SQL/union_and_select.sql
+++ SQL/union_and_select.sql
@@ -49,3 +49,8 @@ SELECT id, 'Leaf' as 'type'
   FROM Tree
  WHERE p_id IS NOT NULL
    AND id NOT IN (SELECT p_id FROM Tree WHERE p_id IS NOT NULL)
+
+-- 176. Second Highest Salary
+-- Write an SQL query to report the second highest salary from the Employee table.
+-- If there is no second highest salary, the query should report null.
+SELECT IFNULL((SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT 1 OFFSET 1), NULL) AS 'SecondHighestSalary'
