b93cdcd641946018b6851d937ddf09a9ca49c049
rami <krhong23@gmail.com>
2022-09-14 23:41:27 +0900

    Union / MySql / 22-09-14
    Runtime: 1332 ms / Memory Usage: 0 MB
    https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions

--- SQL/union.sql
+++ SQL/union.sql
@@ -2,9 +2,10 @@
 -- Write an SQL query to report the first name, last name, city, and state of each person in the Person table.
 -- If the address of a personId is not present in the Address table, report null instead.
 -- Return the result table in any order.
+-- case1. Using LEFT JOIN
 SELECT firstName, lastName, city, state
   FROM Person AS p LEFT JOIN Address AS a ON p.personId = a.personId
-
+-- case2. Using Union
 SELECT firstName, lastName, city, state
   FROM Person AS p, Address AS a
  WHERE p.personId = a.personId
@@ -12,3 +13,17 @@ SELECT firstName, lastName, city, state
 SELECT firstName, lastName, null, null
   FROM Person p
  WHERE p.personId NOT IN (SELECT a.personId FROM Address AS a)
+
+-- 1581. Customer Who Visited but Did Not Make Any Transactions
+-- Write an SQL query to find the IDs of the users who visited without making any transactions and the number of times they made these types of visits.
+-- Return the result table sorted in any order.
+-- case1. Using Subquery
+  SELECT customer_id, COUNT(*) AS count_no_trans
+    FROM Visits
+   WHERE visit_id NOT IN (SELECT visit_id FROM Transactions)
+GROUP BY customer_id
+-- case2. Using Left Join
+  SELECT customer_id, count(v.visit_id) AS count_no_trans
+    FROM Visits AS v LEFT JOIN Transactions AS t ON v.visit_id = t.visit_id
+   WHERE t.visit_id IS NULL
+GROUP BY customer_id
