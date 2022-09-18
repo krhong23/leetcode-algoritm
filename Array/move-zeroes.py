25fb7bec21febb1781d2db0e80e038bfcdb152f5
rami <krhong23@gmail.com>
2022-09-18 11:50:45 +0900

    Two Pointer / Python / 22-09-18
    Runtime: 180 ms / Memory Usage: 15.5 MB
    https://leetcode.com/problems/move-zeroes

--- Array/move-zeroes.py
+++ Array/move-zeroes.py
@@ -1,6 +1,7 @@
-# problem: Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+# 283. Move Zeroes
+# Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 
-
+# case1
 class Solution(object):
     def moveZeroes(self, nums):
         """
@@ -15,3 +16,17 @@ class Solution(object):
                 nums[length - 1] = 0
                 length -= 1
         return nums
+
+# case2. Using two pointer
+class TwoPointerSolution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        t1, t2 = 0, len(nums) - 1
+        while t1 <= t2:
+            if nums[t1] == 0:
+                nums.append(nums.pop(t1))
+                t2 -= 1
+                t1 -= 1
+            t1 += 1
