2f33697b6219e46301ae3df38c0e9202c0e7c9fe
rami <krhong23@gmail.com>
2022-09-19 23:47:19 +0900

    UNION / MySQL / 22-09-19
    Runtime: 1052 ms / Memory Usage: 0 MB
    https://leetcode.com/problems/rising-temperature/

--- SQL/union.sql
+++ SQL/union.sql
@@ -35,3 +35,10 @@ GROUP BY customer_id
     FROM Views
    WHERE author_id = viewer_id
 ORDER BY author_id
+
+-- 197. Rising Temperature
+-- Write an SQL query to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+-- Return the result table in any order.
+SELECT w1.id AS 'Id'
+  FROM Weather w1 JOIN Weather AS w2 ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+   AND w1.temperature > w2.temperature
