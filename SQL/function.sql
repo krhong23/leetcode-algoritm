c01f7837cbebc0c4dabd0db762387e2eeb9b0392
rami <krhong23@gmail.com>
2022-10-11 12:10:08 +0900

    Function(LIMIT) / MySQL / 22-10-11
    Runtime: 412 ms / Memory Usage: 0 MB
    https://leetcode.com/problems/customer-placing-the-largest-number-of-orders/

--- SQL/function.sql
+++ SQL/function.sql
@@ -2,10 +2,16 @@
 -- Write an SQL query to find the daily active user count for a period of 30 days ending 2019-07-27 inclusively.
 -- A user was active on someday if they made at least one activity on that day.
 -- Return the result table in any order.
+-- case 01)
   SELECT activity_date AS day, COUNT(DISTINCT user_id) AS active_users
     FROM Activity
 GROUP BY activity_date
-  HAVING activity_date <= '2019-07-27' AND activity_date > '2019-06-27'
+HAVING activity_date <= '2019-07-27' AND activity_date > '2019-06-27'
+-- case 2)
+  SELECT activity_date AS day, COUNT(DISTINCT(user_id)) AS active_users
+    FROM activity
+   WHERE DATEDIFF('2019-07-27', activity_date) <30 AND activity_date<='2019-07-27'
+GROUP BY 1
 
 -- 1693. Daily Leads and Partners
 -- Write an SQL query that will, for each date_id and make_name, return the number of distinct lead_id's and distinct partner_id's.
@@ -21,3 +27,11 @@ GROUP BY date_id, make_name
     FROM Followers
 GROUP BY user_id
 ORDER BY user_id
+
+-- 586. Customer Placing the Largest Number of Orders
+-- Write an SQL query to find the customer_number for the customer who has placed the largest number of orders.
+-- The test cases are generated so that exactly one customer will have placed more orders than any other customer.
+  SELECT customer_number
+    FROM (SELECT COUNT(order_number) AS CNT, customer_number FROM Orders GROUP BY customer_number) AS T1
+ORDER BY CNT DESC
+   LIMIT 1
