2ec824b99ff1eabfa437e05349fe5ae390f6ef48
rami <krhong23@gmail.com>
2022-10-11 12:51:01 +0900

    Two Pointer / Python / 22-10-11
    Runtime: 61 ms / Memory Usage: 13.9 MB
    https://leetcode.com/problems/remove-nth-node-from-end-of-list/

--- LinkedList/remove-nth-node-from-end-of-list.py
+++ LinkedList/remove-nth-node-from-end-of-list.py
@@ -1,10 +1,12 @@
-# problem: Given the head of a linked list, remove the nth node from the end of the list and return its head.
+# 19. Remove Nth Node From End of List
+# Given the head of a linked list, remove the nth node from the end of the list and return its head.
 
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# case 1
 class Solution(object):
     def removeNthFromEnd(self, head, n):
         """
@@ -29,4 +31,18 @@ class Solution(object):
                 removeNode = removeNode.next
             removeNode.next = back.next
 
+        return head
+
+# case 2. two pointer
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        slow = fast = head
+        for _ in range(n):
+            fast = fast.next
+        if not fast:
+            return head.next
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+        slow.next = slow.next.next
         return head
