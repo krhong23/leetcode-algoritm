bf4a1a9047730a660e5a136c5a093de012d0bc43
rami <krhong23@gmail.com>
2022-10-11 12:15:20 +0900

    Function(MIN) / MySQL / 22-10-11
    Runtime: 1056 ms / Memory Usage: 0 MB
    https://leetcode.com/problems/game-play-analysis-i/

--- SQL/function.sql
+++ SQL/function.sql
@@ -35,3 +35,10 @@ ORDER BY user_id
     FROM (SELECT COUNT(order_number) AS CNT, customer_number FROM Orders GROUP BY customer_number) AS T1
 ORDER BY CNT DESC
    LIMIT 1
+
+-- 511. Game Play Analysis I
+-- Write an SQL query to report the first login date for each player.
+-- Return the result table in any order.
+  SELECT player_id, MIN(event_date) AS first_login
+    FROM Activity
+GROUP BY player_id
