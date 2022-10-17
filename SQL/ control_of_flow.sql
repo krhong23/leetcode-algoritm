83632564ed4515f92b874f4a970e1bc43f0b69d4
rami <krhong23@gmail.com>
2022-10-17 12:06:19 +0900

    Control of Flow / MySQL / 22-10-17
    Runtime: 1960 ms / Memory Usage: 0 MB
    https://leetcode.com/problems/top-travellers/

--- SQL/ control_of_flow.sql
+++ SQL/ control_of_flow.sql
@@ -9,3 +9,15 @@
              END) AS 'capital_gain_loss'
     FROM Stocks
 GROUP BY stock_name
+
+-- 1407. Top Travellers
+-- Write an SQL query to report the distance traveled by each user.
+-- Return the result table ordered by travelled_distance in descending order,
+-- if two or more users traveled the same distance, order them by their name in ascending order.
+  SELECT name, IFNULL(distance, 0) AS 'travelled_distance'
+    FROM Users AS a
+         LEFT JOIN (  SELECT user_id, SUM(distance) 'distance'
+                        FROM Rides
+                    GROUP BY user_id) AS b
+                ON a.id = b.user_id
+ORDER BY distance DESC, name ASC
