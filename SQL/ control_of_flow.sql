2bf55ee662c900efd394d0a6d212c6611a67a426
rami <krhong23@gmail.com>
2022-10-17 12:19:33 +0900

    Control of Flow / MySQL / 22-10-17
    Runtime: 1757 ms / Memory Usage: 0 MB
    https://leetcode.com/problems/market-analysis-i/

--- SQL/ control_of_flow.sql
+++ SQL/ control_of_flow.sql
@@ -21,3 +21,13 @@ GROUP BY stock_name
                     GROUP BY user_id) AS b
                 ON a.id = b.user_id
 ORDER BY distance DESC, name ASC
+
+-- 1158. Market Analysis I
+-- Write an SQL query to find for each user, the join date and the number of orders they made as a buyer in 2019.
+-- Return the result table in any order.
+  SELECT a.user_id AS 'buyer_id', a.join_date, IFNULL(COUNT(b.order_id), 0) AS 'orders_in_2019'
+    FROM Users AS a
+         LEFT JOIN Orders AS b ON a.user_id = b.buyer_id
+     AND order_date >= '2019-01-01'
+     AND order_date < '2020-01-01'
+GROUP BY a.user_id
