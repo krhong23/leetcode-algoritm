ae520f8aa11d905b603452a15ad8f42bb5b825f4
rami <krhong23@gmail.com>
2022-10-23 18:59:20 +0900

    Where / MySQL / 22-10-23
    Runtime: 388 ms / Memory Usage: 14.7 MB
    https://leetcode.com/problems/actors-and-directors-who-cooperated-at-least-three-times/

--- SQL/where.sql
+++ SQL/where.sql
@@ -6,3 +6,10 @@
 GROUP BY email
   HAVING COUNT(*) > 1;
 
+-- 1050. Actors and Directors Who Cooperated At Least Three Times
+-- Write a SQL query for a report that provides the pairs (actor_id, director_id) where the actor has cooperated with the director at least three times.
+-- Return the result table in any order.
+  SELECT actor_id, director_id
+    FROM ActorDirector
+GROUP BY actor_id, director_id
+  HAVING COUNT(*) >= 3
